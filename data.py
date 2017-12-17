5c130727dcd426e3249425195cd305f08427dfe9
张建功 <zjg@demian.local>
2017-12-17 23:38:54 +0800

    flask

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles=[
+        {
+            'id':1,
+            'title':'Article One',
+            'body':'I am happy to practice flask, I like coding.',
+            'author':'facebook',
+            'create_date':'12-16-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article two',
+            'body': 'I am happy to practice flask, I like coding.',
+            'author': 'hulu',
+            'create_date': '12-17-2017'
+        },
+        {
+            'id':3,
+            'title':'Article Three',
+            'body':'I am happy to practice flask, I like coding.',
+            'author':'google',
+            'create_date':'12-18-2017'
+        }
+    ]
+    return articles
